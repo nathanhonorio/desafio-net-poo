5290f02637e6541b94076a71ceb44dd2309d8421
Nathan Honório <nathanhonorio.dev@gmail.com>
2023-11-02 23:02:10 -0300

    Solução desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphone13 = new Iphone("2199999999", "13", "IMEI98989721", 256);
+iphone13.InstalarAplicativo("whatsapp");
+iphone13.Ligar();
+iphone13.ReceberLigacao();
+
+Smartphone nokiaWindowsPhone = new Nokia("2199999999", "Windows phone", "I1208138192",16);
+
+nokiaWindowsPhone.Ligar();
+nokiaWindowsPhone.ReceberLigacao();
+nokiaWindowsPhone.InstalarAplicativo("Twitter");
